a31cfb7b7d1dd627ce69247efe3d5bed1a866b3e
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2026-06-01 10:04:26 +0530

    JS arrayvs Object

---
+++ class-4-DT3/data.js
@@ -0,0 +1,6 @@
+//create js - object
+let arr=[]
+let a  ={}
+//how to find data type
+console.log(typeof arr) //object
+console.log(typeof a)   //object
---
+++ class-4-DT3/two.js
@@ -0,0 +1,11 @@
+let eids=[101,101,101,102,101,101]
+//indexing 0    1   2  3    4   5
+let emp={
+    'eid':101,
+    'email':"Rahul1@gmail.com",
+    'email':"Rahul2@gmail.com",
+    'email':"Rahul3@gmail.com",
+    'email':"Rahul4@gmail.com",
+    'email':"Rahul5@gmail.com",
+}
+console.log(emp)
